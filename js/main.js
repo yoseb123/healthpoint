b659bfec43433f763c010ca84c809adfc83d4f3c
esc11 <esc11@uw.edu>
2015-12-06 02:46:06 -0800

    Added basic information + embedded Google maps, no formatting

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// var $ = require('jquery'),
-//     data = require('./data/data'),
-//     constants = require('./lib/constants'),
-//     angular = require('angular'),
-//     angularUiRouter = require("angular-ui-router");
+ // var $ = require('jquery'),
+ //     data = require('./data/data'),
+ //     constants = require('./lib/constants'),
+ //     angular = require('angular'),
+ //     angularUiRouter = require("angular-ui-router");
 //ui-router/release/
-var myApp = angular.module('MyApp', ['ui.router'])
+var myApp = angular.module('MyApp', ['ui.router', 'ngSanitize'])
 	.config(function($stateProvider){
 	$stateProvider
 		.state('home', {
@@ -20,10 +20,10 @@ var myApp = angular.module('MyApp', ['ui.router'])
 			controller: 'SearchCtrl'
 		})	
 		.state('detail', {
-			url: '/hospital/{id}',//need specific id variable name
+			url: '/hospital/{id}', //need specific id variable name
 			templateUrl: 'partials/hospital-detail.html',
 			controller: 'DetailCtrl'
-		})
+		});
 
 
 })
@@ -39,13 +39,47 @@ var myApp = angular.module('MyApp', ['ui.router'])
 
 }])
 
-.controller('DetailCtrl', ['$scope', '$http',function($scope, $http) {
+.controller('DetailCtrl', ['$scope', '$http', '$sce',function($scope, $http, $sce) {
 
 	console.log("in detail");
 
-}]);
+	$http.get('js/data/hospitals.json').then(function(response) {
+        $scope.hospitals = response.data.data;
+        // $scope.hospital = $filter('filter')($scope.hospitals, { // filter the array
+        //     8: $stateParams.id
+        // }, true)[0]; // save the 0th result
+		$scope.testHospital = response.data.data[91];
+        console.log($scope.testHospital);
+    });
 
-console.log(myApp);
+    $scope.lowercase = function(string) {
+        return string.replace(/\w\S*/g, function(word) {
+        	if (word.length > 2) {
+        		return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+        	} else {
+        		return word;
+        	}
+        });
+    };
+
+    $scope.cityState = function(hospital) {
+    	var city = hospital[11].charAt(0).toUpperCase() + hospital[11].substr(1).toLowerCase();
+    	var state = hospital[12];
+    	return city + ', ' + state; 
+    };
+
+    $scope.phone = function(number) {
+    	return '(' + number.substr(0,3) + ')-' + number.substr(3,3) + '-' + number.substr(6,4); 
+    };
+
+    $scope.mapURL = function(hospital) {
+        var name = hospital[9]
+    	var url = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyAnezpngkfubjHIWnrZNCmhGv-_IzyLjMs&q='
+    	+ name;
+    	return $sce.trustAsResourceUrl(url);
+    };
+
+}])
 
 // data.hospitals.data.forEach(function(hosp) {
 //     console.log(hosp[constants.DATA_START_INDEX]);
