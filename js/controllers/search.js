cdc7b39b4d59222303ef96da3fd74b1471922c11
esc11 <esc11@uw.edu>
2015-12-11 03:56:16 -0800

    Patient survey ratings now in search results

--- js/controllers/search.js
+++ js/controllers/search.js
@@ -1,63 +1,54 @@
 'use strict';
 
 // search controller
-App.controller('SearchCtrl', ['$scope', '$http','homeSearch',function($scope, $http, homeSearch) {
+App.controller('SearchCtrl', ['$scope', '$http','homeSearch','$filter',function($scope, $http, homeSearch, $filter) {
     $scope.searchQuery = homeSearch.toSearch;
     $scope.sortingCriteria = '';
     $http.get('js/data/data.json').then(function(response) {
         $scope.hospitals = []; 
         $scope.hospitalsDataSet = response.data;
-
-        $scope.updateReviews = function(hospital) {
-            // query for all reviews for the hospital with id
-            var Post = Parse.Object.extend('Post');
-            var query = new Parse.Query(Post);
-            query.equalTo('hospitalId', hospital[1]);
-            query.find({
-                success: function(results) {
-                    $scope.$apply(function () {
-                        $scope.reviews = results;
-                    });
-                    $scope.setAverageRatings(hospital);
-                },
-                error: function(error) {
-                    console.log('Error: ' + error.code + 
-                        ' ' + error.message);
-                }
-            });
-        };
-
-        // sets the average ratings by
-        // going through the reviews to take 
-        // the average of all rating categories
-        $scope.setAverageRatings = function(hospital) {
-            var avgClean = 0,
-                avgQuiet = 0,
-                avgPain = 0,
-                avgDoctor = 0,
-                avgNurse = 0;
-
-            for (var i = 0; i < $scope.reviews.length; i++) {
-                avgClean += $scope.reviews[i].get('clean');
-                avgQuiet += $scope.reviews[i].get('quiet');
-                avgPain += $scope.reviews[i].get('pain');
-                avgDoctor += $scope.reviews[i].get('doctor');
-                avgNurse += $scope.reviews[i].get('nurse');
-            }
-
-            hospital.push(Math.round((avgClean / $scope.reviews.length)*10)/10);
-            hospital.push(Math.round((avgQuiet / $scope.reviews.length)*10)/10);
-            hospital.push(Math.round((avgPain / $scope.reviews.length)*10)/10);
-            hospital.push(Math.round((avgDoctor / $scope.reviews.length)*10)/10);
-            hospital.push(Math.round((avgNurse / $scope.reviews.length)*10)/10);
-
-        };
-
         for (var i = 0; i < $scope.hospitalsDataSet[0].data.length; i++) {
-        	var hospital = $scope.hospitalsDataSet[0].data[i]
+        	var hospital = $scope.hospitalsDataSet[0].data[i];
             $scope.hospitals.push(hospital);
-            $scope.updateReviews(hospital)
         };
+    });
+
+    $http.get('js/data/surveys.json').then(function(response) {
+        $scope.surveys = response.data.data;
+        for (var i = 0; i < $scope.hospitals.length; i++) {
+        	var hospital = $scope.hospitalsDataSet[0].data[i];
+	        var cleanRating = $filter('filter')($scope.surveys, function(survey) {
+	        	if (survey[11] == 'H_CLEAN_STAR_RATING' && survey[8] == hospital[8]) {
+	                hospital.push(survey[13]);
+	            }
+	        }, true)[0];
+
+	        var quietRating = $filter('filter')($scope.surveys, function(survey) {
+	            if (survey[11] == 'H_QUIET_STAR_RATING' && survey[8] == hospital[8]) {
+	                hospital.push(survey[13]);
+	            }
+	        }, true)[0];
+
+	        var painRating = $filter('filter')($scope.surveys, function(survey) {
+	            if (survey[11] == 'H_COMP_4_STAR_RATING' && survey[8] == hospital[8]) {
+	                hospital.push(survey[13]);
+	            }
+	        }, true)[0];
+
+	        var doctorRating = $filter('filter')($scope.surveys, function(survey) {
+	            if (survey[11] == 'H_COMP_2_STAR_RATING' && survey[8] == hospital[8]) {
+	                hospital.push(survey[13]);
+	            }
+	        }, true)[0];
+
+	        var nurseRating = $filter('filter')($scope.surveys, function(survey) {
+	            if (survey[11] == 'H_COMP_1_STAR_RATING' && survey[8] == hospital[8]) {
+	                hospital.push(survey[13]);
+	            }
+	        }, true)[0];
+
+	        $scope.hospitalsDataSet[0].data[i] = hospital;
+	    };
     });
 
 }]);
